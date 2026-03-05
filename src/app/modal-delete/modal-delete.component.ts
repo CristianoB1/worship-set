bb0a880111396adb9cf464b8650e76d272d1be7a
Cristiano de Oliveira Barbosa <cristianobr188@gmail.com>
2026-03-05 00:58:56 -0300

    teste del

--- src/app/modal-delete/modal-delete.component.ts
+++ src/app/modal-delete/modal-delete.component.ts
@@ -3,6 +3,7 @@ import { MatDialogContent, MatDialogRef } from "@angular/material/dialog";
 import { CommonModule } from "@angular/common";
 import { MusicasService } from "../musicas/musicas.service";
 import { FormsModule } from '@angular/forms';
+import { Musicas } from '../musicas/musicas';
 
 @Component({
   selector: 'app-modal-delete',
@@ -13,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 })
 export class ModalDeleteComponent implements OnInit {
 
+  musicas: Musicas[] = [];
   erroMsg: string = '';
   id!: number;
 
@@ -25,27 +27,23 @@ export class ModalDeleteComponent implements OnInit {
   }
 
   deleteMusica() {
-    console.log('deletar musica com id:', this.id);
+  console.log('deletar musica com id:', this.id);
 
-    this.musicaService.deleteMusica(this.id).subscribe({
-      next: () => {
-        console.log('Música deletada com sucesso');
-        // setTimeout(() => {
-        // window.location.reload()}, 2000);
-        setTimeout(() => {
-          this.musicaService.getMusicas().subscribe({
-            next: (data) => {
-              console.log('Músicas atualizadas:', data);
-            }
-          });
-        }, 6000);
-      },
-      error: (err: any) => {
-        console.error(err)
-        this.erroMsg = 'Erro ao deletar a música. Verifique o ID e tente novamente.';
-      },
-    });
-  }
+  this.musicaService.deleteMusica(this.id).subscribe({
+    next: () => {
+      console.log('Música deletada com sucesso');
+
+      this.musicaService.getMusicas().subscribe((data) => {
+        this.musicas = data; // ATUALIZA A TELA
+      });
+
+    },
+    error: (err: any) => {
+      console.error(err)
+      this.erroMsg = 'Erro ao deletar a música. Verifique o ID e tente novamente.';
+    },
+  });
+}
 
   fecharModal() {
     this.dialogRef.close();
